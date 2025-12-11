0efdedead7acf12cbbba567990a292702154d57d
Deepankar Sah <deepankerrocking@gmail.com>
2025-12-11 21:55:51 +0530

    updated

--- PROJECT/BOOK-STORE-API/index.js
+++ PROJECT/BOOK-STORE-API/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const connectToDB = require("./database/db");
 const express = require("express");
+
+const bookRoutes = require("./routes/bookRoute");
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Connect to database
 connectToDB();
@@ -9,8 +13,17 @@ connectToDB();
 // Middleware
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+// Home Route
+app.get("/api/v1/", async (req, res) => {
+  res.status(201).json({
+    sucess: true,
+    message: "Welcome to book store",
+  });
+});
 
-app.listen(() => {
+// Book Routes
+app.use("/api/v1/books", bookRoutes);
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
