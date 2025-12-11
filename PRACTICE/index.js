5577983f70cdf0818ccdfa644edc5b0e0d8e9f02
Deepankar Sah <deepankerrocking@gmail.com>
2025-12-11 15:21:11 +0530

    env port added

--- PRACTICE/index.js
+++ PRACTICE/index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to home page",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
